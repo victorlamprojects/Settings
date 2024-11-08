547c418c5dd81d08606b26bd8651ebfdd0d813eb
victorlamprojects <victorlamprojects@gmail.com>
2024-11-09 01:56:30 +0800

    updated NVIM setup docs

--- Linux Setup/nvim/nvim/lua/plugins/git.lua
+++ Linux Setup/nvim/nvim/lua/plugins/git.lua
@@ -12,9 +12,9 @@ return {
         config = function()
             vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", {desc="[G]it [B]lame"})
             vim.keymap.set("n", "<leader>gA", ":Git add .<cr>", {desc = "[G]it Add [A]ll"})
-            vim.keymap.set("n", "<leader>ga", "Git add", {desc = "[G]it [A]dd"})
-            vim.keymap.set("n", "<leader>gc", ":Git commit", {desc = "[G]it [C]ommit"})
-            vim.keymap.set("n", "<leader>gp", "Git push", {desc = "[G]it [P]ush"})
+            vim.keymap.set("n", "<leader>ga", ":Git add<cr>", {desc = "[G]it [A]dd"})
+            vim.keymap.set("n", "<leader>gc", ":Git commit<cr>", {desc = "[G]it [C]ommit"})
+            vim.keymap.set("n", "<leader>gP", ":Git push<cr>", {desc = "[G]it [P]ush"})
         end
     }
 }
