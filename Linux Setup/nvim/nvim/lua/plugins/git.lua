a02953617a7da28c3fb12cc7621537bfb144fb2e
victorlamprojects <65209724+victorlamprojects@users.noreply.github.com>
2024-12-14 17:12:35 +0800

    Update git.lua

--- Linux Setup/nvim/nvim/lua/plugins/git.lua
+++ Linux Setup/nvim/nvim/lua/plugins/git.lua
@@ -10,11 +10,14 @@ return {
     {
         "tpope/vim-fugitive",
         config = function()
+            vim.keymap.set("n", "<leader>gs", ":Git<cr>", {desc = "[G]it [S]tatus"})
             vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", {desc="[G]it [B]lame"})
             vim.keymap.set("n", "<leader>gA", ":Git add .<cr>", {desc = "[G]it Add [A]ll"})
             vim.keymap.set("n", "<leader>ga", ":Git add<cr>", {desc = "[G]it [A]dd"})
             vim.keymap.set("n", "<leader>gc", ":Git commit<cr>", {desc = "[G]it [C]ommit"})
             vim.keymap.set("n", "<leader>gP", ":Git push<cr>", {desc = "[G]it [P]ush"})
+            vim.keymap.set("n", "<leader>gl", ":Git log<cr>", {desc = "[G]it [L]og"})
+            vim.keymap.set("n", "<leader>gd", ":Git diff<cr>", {desc = "[G]it [D]iff"})
         end
     }
 }
