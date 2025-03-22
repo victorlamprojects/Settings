d95fa5ea62543e65950e13b211671479309b26ba
victorlamprojects <65209724+victorlamprojects@users.noreply.github.com>
2025-03-22 17:48:56 +0800

    Update colorscheme.lua

--- Linux Setup/nvim/nvim/lua/plugins/colorscheme.lua
+++ Linux Setup/nvim/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,50 @@
 return {
     "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {
-        transparent = true,
-    }
-    config = function ()
+        term_colors = true,
+        transparent_background = true,
+        dim_inactive = {
+            enabled = false,
+            shade = "dark",
+            percentage = 0.15 -- % of the shade applied on the inactive window
+        },
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            treesitter = true,
+            telescope = true,
+            mason = true,
+            which_key = true,
+            fidget = true,
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                },
+                unerlines = {
+                    errors =  { "underline" },
+                    hints =  { "underline" },
+                    warnings =  { "underline" },
+                    information =  { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
+            mini = {
+                enabled = true,
+                indentscope_color = "",
+            },
+        },
+    },
+    config = function (_, opts)
+        require("catppuccin").setup(opts)
         vim.cmd.colorscheme "catppuccin"
     end
 }
