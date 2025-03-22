9d0b032b08938967fc4c968b22c326a0a692f4e7
victorlamprojects <65209724+victorlamprojects@users.noreply.github.com>
2025-03-22 17:28:21 +0800

    added transparent nvim

--- Linux Setup/nvim/nvim/lua/plugins/colorscheme.lua
+++ Linux Setup/nvim/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+        transparent = true,
+    }
     config = function ()
         vim.cmd.colorscheme "catppuccin"
     end
