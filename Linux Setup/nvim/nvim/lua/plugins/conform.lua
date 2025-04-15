24642ddaa800a74aa27027246fde2228273628c7
victorlamprojects <65209724+victorlamprojects@users.noreply.github.com>
2025-04-15 15:20:57 +0800

    Update conform.lua

--- Linux Setup/nvim/nvim/lua/plugins/conform.lua
+++ Linux Setup/nvim/nvim/lua/plugins/conform.lua
@@ -1,11 +1,25 @@
 return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>p",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
         formatters_by_ft = {
             lua = { "stylua" },
             python = { "isort", "black" },
-            javascript = { "prettierd", "prettier", stop_after_first = true }, 
+            javascript = { "prettierd", "prettier", stop_after_first = true },
             javascriptreact = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
             typescriptreact = { "prettierd", "prettier", stop_after_first = true },
@@ -15,6 +29,9 @@ return {
             yaml = { "prettierd", "prettier", stop_after_first = true },
             json = { "fixjson", "prettier" },
             xml = { "xmlformatter" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
         },
         format_on_save = { timeout_ms = 500 },
     },
