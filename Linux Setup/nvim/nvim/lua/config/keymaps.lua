3b51b263a7419f5713475b1954ca1da101256b14
victorlamprojects <65209724+victorlamprojects@users.noreply.github.com>
2025-04-15 14:22:15 +0800

    Update keymaps.lua

--- Linux Setup/nvim/nvim/lua/config/keymaps.lua
+++ Linux Setup/nvim/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>h", "<C-w><C-h>", { desc = "Move focus to the left 
 vim.keymap.set("n", "<leader>l", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<leader>j", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<leader>k", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>s", ":w<cr>", { desc = "Saved file" })
 vim.keymap.set("n", "<leader>q", ":q<cr>", { desc = "Move focus to the upper window" })
 
 -- Split Windows
